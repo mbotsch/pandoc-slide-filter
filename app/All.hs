86ff15762f922a1004acef60c2dc4563d674a6ef
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2018-05-24 15:49:56 +0200

    added all-filter which combines all filters into one ;)

---
+++ app/All.hs
@@ -0,0 +1,30 @@
+#!/usr/bin/env runhaskell
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Control.Monad
+import Text.Pandoc.JSON
+import Text.Pandoc.Walk
+import Text.Pandoc.Generic
+import Text.Pandoc.Util.Filter.Quiz
+import Text.Pandoc.Util.Filter.Cols
+import Text.Pandoc.Util.Filter.Media
+import Text.Pandoc.Util.Filter.Styling
+
+main :: IO ()
+main = toJSONFilter $
+    fmap (walk (concatMap quiz) :: Pandoc -> Pandoc) .
+    (walkM (fmap concat . mapM styling) :: Pandoc -> IO Pandoc) <=<
+    (walkM (fmap concat . mapM media) :: Pandoc -> IO Pandoc) .
+    (walk (concatMap quiz) :: Pandoc -> Pandoc) .
+    (topDown cols :: Pandoc -> Pandoc)
+
+clean :: Block -> [Block]
+clean (Plain          []) = []
+clean (Para           []) = []
+clean (LineBlock      []) = []
+clean (BlockQuote     []) = []
+clean (OrderedList _  []) = []
+clean (BulletList     []) = []
+clean (DefinitionList []) = []
+clean x                   = [x]
+
